3aacf7232f896cfbf2a6e0fb3bd185bab3a593c9
Danilo Del Collado <daniloddc.92@gmail.com>
2022-08-30 23:44:01 -0300

    Added blur filter on New Task Input.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'taskProject';
+  task: string = 'asd';
+  imageUrl : string = '../img/trash.png';
+  currentClasses: Record<string, boolean> = {};
+  isSpecial = true;
+
+
+  addTask(newTask: string) {
+    this.task = (newTask);
+    console.log(' this task: ' + this.task + '!');
+  }
+
+  setCurrentClasses() {
+    this.currentClasses =  {
+      special:  this.isSpecial
+    };
+  }
 }
